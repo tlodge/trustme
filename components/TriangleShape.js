681ccd97c39a7cc1c14336bca840924bad173f40
Tom Lodge <tlodge@gmail.com>
2022-01-18 19:51:35 +0000

    cp commit

--- components/TriangleShape.js
+++ components/TriangleShape.js
@@ -18,7 +18,7 @@ const TriangleShape = (props) => {
         scene = new THREE.Scene();
         scene.background = new THREE.Color( 0xffffff );
         
-        //helper = new THREE.GridHelper( 160, 100 );
+       //helper = new THREE.GridHelper( 160, 100 );
         //helper.rotation.x = Math.PI / 2;
         //scene.add( helper );
         
@@ -70,6 +70,7 @@ const TriangleShape = (props) => {
             for ( let j = 0; j < shapes.length; j ++ ) {
                 const shape = shapes[ j ];
                 const geometry = new THREE.ExtrudeGeometry(shape, {depth: 15,bevelEnabled: false});
+                //geometry.center();
                 const mesh = new THREE.Mesh( geometry, material );
                 group.add( mesh );
             }
@@ -94,17 +95,23 @@ const TriangleShape = (props) => {
 
             groups[chapter] = addShapes(path, chapter*4, Object.keys(groups).length > 1 ? 0x4E89F8 : colour[0]);
             parent.add(groups[chapter]);
-            parent.position.z = -(chapter*2);
+            console.log("Adding chapter", chapter, "x", groups[chapter].position.x, "y", groups[chapter].position.y, "z", groups[chapter].position.z);
+
+            parent.position.y = 10; //how close to top (make vary with chapter)
+            parent.position.x = 0; //intercept with axis
+            parent.position.z = -1;
+            parent.rotation.x =  Math.PI / 2;
         };
         
         shapeRef.current.appendChild(renderer.domElement);
         const _controls = new OrbitControls( camera, renderer.domElement );
+     
         _controls.screenSpacePanning = true;
 
         const animate =  ()=> {
             requestAnimationFrame(animate);
-            parent.rotation.x += 0.002;
-            parent.rotation.y += 0.002;
+            parent.rotation.z += 0.002;
+            //parent.rotation.y += 0.002;
            
             //parent.rotation.z += 0.002;
             renderer.render(scene, camera);
